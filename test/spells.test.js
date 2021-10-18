c18e8836304708d66e3a879e879808e39672a1ff
bpeteradams <bpeteradams@gmail.com>
2021-10-18 09:31:16 -0400

    wrote first test for the find function on the spell object

--- test/spells.test.js
+++ test/spells.test.js
@@ -3,3 +3,9 @@ const spells = require('../lib/spells');
 test('Spells is an object', () => {
   expect(typeof spells).toBe('object')
 })
+test('Spell has an attribute, "find"', () => {
+  expect(spells.find).toBeDefined()
+})
+test('Spell.find is a function', () => {
+  expect(typeof spells.find).toBe('function')
+})
