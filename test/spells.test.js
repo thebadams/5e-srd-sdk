fe1b438e7578ad11f07d6b82855e0f654af4f024
bpeteradams <bpeteradams@gmail.com>
2021-10-18 09:46:26 -0400

    restructured tests and tested for an endpoint attribute on the spells object

--- test/spells.test.js
+++ test/spells.test.js
@@ -1,11 +1,23 @@
 const spells = require('../lib/spells');
-
-test('Spells is an object', () => {
+describe('Spell', () => {
+  test('Spells is an object', () => {
   expect(typeof spells).toBe('object')
-})
-test('Spell has an attribute, "find"', () => {
-  expect(spells.find).toBeDefined()
-})
-test('Spell.find is a function', () => {
-  expect(typeof spells.find).toBe('function')
+  })
+  describe('Spell endpoint Attribute', () => {
+    test('Spell has an attrubute, endpoint', () => {
+    expect(spells.endpoint).toBeDefined()
+    })
+    test('Spell endpoint attribute should be a string equalling "https://www.dnd5eapi.co/api/spells"', () => {
+      expect(typeof spells.endpoint).toBe('string')
+      expect(spells.endpoint).toEqual('https://www.dnd5eapi.co/api/spells')
+    })
+  })
+  describe('Spell find Attribute', () => {
+    test('Spell has an attribute, "find"', () => {
+    expect(spells.find).toBeDefined()
+    })
+    test('Spell.find is a function', () => {
+    expect(typeof spells.find).toBe('function')
+    })
+  })
 })
